f4c9ec89165b4943dfa3118f46b639f47f4b36d9
mohsen <mohzia1380@gmail.com>
2022-11-03 18:00:52 +0330

    customize admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
+    list_filter = ('status', 'publish', 'created','author')
+    search_field =('title','body')
+    date_hierarchy = 'publish'
+    ordering = ('-publish','status')
+    list_editable =('status',)
+    list_display_links = ('slug',)
--- blog/models.py
+++ blog/models.py
@@ -13,12 +13,13 @@ class Post(models.Model):
     slug=models.SlugField(max_length=250,unique_for_date='publish')
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
     body=models.TextField()
+    publish= models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=10,choices=STATUS_CHOICE,default='published')
 
     class Meta:
-        ordering=('-created',)
+        ordering=('-publish',)
 
     def __str__(self):
         return self.title
