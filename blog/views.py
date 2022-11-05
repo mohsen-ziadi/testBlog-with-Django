2947f40a974498fce97a5c3df45726f47b1d4f21
mohsen <mohzia1380@gmail.com>
2022-11-05 14:30:14 +0330

    create paginator

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -9,8 +10,17 @@ def index(requset):
 
 def post_list(requset):
     posts = Post.objects.filter(status="published")
-    return render(requset,'blog/post/list.html',{'posts':posts})
+    paginator = Paginator(posts,8)
+    page = requset.GET.get('page')
+
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+    except EmptyPage:
+        posts=paginator.page(paginator.num_pages)
+    return render(requset,'blog/post/list.html',{'posts':posts,'page':page})
 
 def post_detail(requset,post,pk):
     post = get_object_or_404(Post,slug=post,id=pk)
-    return render(requset,'blog/post/detail.html',{"post":postgi})
+    return render(requset,'blog/post/detail.html',{"post":post})
