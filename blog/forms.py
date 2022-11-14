4c8d069baf21e54134b8d5b56687225dc8414006
mohsen <mohzia1380@gmail.com>
2022-11-14 09:06:24 +0330

    add frist code

--- blog/forms.py
+++ blog/forms.py
@@ -10,3 +10,8 @@ class AccountForm(forms.Form):
     gender = forms.ChoiceField(choices=GENDER_CHOICE,widget=forms.RadioSelect)
     phone = forms.CharField(max_length=13)
     address = forms.CharField(max_length=250,widget=forms.Textarea)
+
+
+class ShareForm(forms.Form):
+    name_share=forms.CharField(max_length=25,required=True,label="نام و نام خانوادگی")
+    email_share = forms.EmailField(required=True,label="ایمیل")
